cbc244e1803e83c1cd47589a7986bcc4fc7f211d
prao <pch5521@gmail.com>
2023-11-17 18:24:30 +0900

    test(MemberService): 회원가입 테스트

---
+++ src/test/java/prao/praospring/service/MemberServiceTest.java
@@ -0,0 +1,26 @@
+package prao.praospring.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import prao.praospring.domain.Member;
+
+class MemberServiceTest {
+
+    MemberService memberService = new MemberService();
+
+    @Test
+    void 회원가입 () {
+        //given
+        Member member = new Member();
+        member.setName("hello");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+}
