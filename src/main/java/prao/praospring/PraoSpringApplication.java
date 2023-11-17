d506588adb7f2f0a2bfc670dce91c0a92257765d
prao <pch5521@gmail.com>
2023-11-17 13:41:19 +0900

    setup: 스프링부트 프로젝트 초기 설정

---
+++ src/main/java/prao/praospring/PraoSpringApplication.java
@@ -0,0 +1,13 @@
+package prao.praospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/prao/praospring/PraoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package prao.praospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
