ecc75bb634699c64786a75642c248edf201d6925
prao <pch5521@gmail.com>
2023-11-17 16:14:45 +0900

    feat(MemoryMemberRepository): 회원 정보 저장소를 구현한 클래스 생성
    - save: store에 id와 member 객체를 저장 후 member 객체 반환
    - findById: id를 받아서 store를 순회하며 id에 해당하는 Member 반환(Optional)
    - findByName: store에 저장된 member를 순회하며 member의 name과 입력받은 name이 같은 값이 있다면 반환
    - findAll: store의 member를 ArrayList로 반환

---
+++ src/main/java/prao/praospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,38 @@
+package prao.praospring.repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import prao.praospring.domain.Member;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static final Map<Long, Member> store = new HashMap<>();
+    private long sequence = 0L;
+
+    @Override
+    public Member save(final Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(final Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(final String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
