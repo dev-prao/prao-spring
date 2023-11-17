fbcf0662bc1ea983392d841edcea519f9b494939
prao <pch5521@gmail.com>
2023-11-17 18:28:06 +0900

    test(MemberService): 중복 회원 예외 테스트(try-catch)

--- src/test/java/prao/praospring/service/MemberServiceTest.java
+++ src/test/java/prao/praospring/service/MemberServiceTest.java
@@ -23,4 +23,24 @@ class MemberServiceTest {
         Member findMember = memberService.findOne(saveId).get();
         assertThat(member.getName()).isEqualTo(findMember.getName());
     }
+
+    @Test
+    void 중복_회원_예외 () {
+        //given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        //when
+        memberService.join(member1);
+        try {
+            memberService.join(member2);
+            fail();
+        } catch (IllegalStateException e) {
+            //then
+            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+        }
+    }
 }
