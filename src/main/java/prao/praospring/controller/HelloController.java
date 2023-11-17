a74c205d8cdf20526b6277a6ee208a3b51f86598
prao <pch5521@gmail.com>
2023-11-17 14:55:11 +0900

    feat(HelloController): @GetMapping("hello") 기능 구현

---
+++ src/main/java/prao/praospring/controller/HelloController.java
@@ -0,0 +1,15 @@
+package prao.praospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "spring 공부해라!!!!");
+        return "hello";
+    }
+}
