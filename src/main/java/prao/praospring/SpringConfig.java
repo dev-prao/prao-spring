aa477f064de3a2e87e1a87ed9cec6d9fa79b6113
prao <pch5521@gmail.com>
2023-11-20 14:30:37 +0900

    refactor(SpringConfig): memberRepository 반환 값 변경
    - MemoryMemberRepository -> JdbcMemberRepository(dataSource)
    - 직접 구현한 JDBC 저장소로 변경

--- src/main/java/prao/praospring/SpringConfig.java
+++ src/main/java/prao/praospring/SpringConfig.java
@@ -1,13 +1,22 @@
 package prao.praospring;
 
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import prao.praospring.repository.JdbcMemberRepository;
 import prao.praospring.repository.MemberRepository;
-import prao.praospring.repository.MemoryMemberRepository;
 import prao.praospring.service.MemberService;
 
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(final DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +25,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
