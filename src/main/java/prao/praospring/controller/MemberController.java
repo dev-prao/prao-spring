12c6967382f2c46c571065795296288a33db8f7b
prao <pch5521@gmail.com>
2023-11-20 13:31:19 +0900

    feat(MemberController): 조회 기능 구현
    - memberService의 findMembers를 이용해서 Member 리스트를 생성하고 model에 해당 리스트를 담은 후 memberList.html를 반환한다.

--- src/main/java/prao/praospring/controller/MemberController.java
+++ src/main/java/prao/praospring/controller/MemberController.java
@@ -1,7 +1,9 @@
 package prao.praospring.controller;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import prao.praospring.domain.Member;
@@ -22,7 +24,7 @@ public class MemberController {
         return "members/createMemberForm";
     }
 
-    @PostMapping("/memberes/new")
+    @PostMapping("/members/new")
     public String create(MemberForm form) {
         Member member = new Member();
         member.setName(form.getName());
@@ -30,5 +32,12 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
