0cefaf43b5c7c09de3d4245c0727014a43c8ca26
prao <pch5521@gmail.com>
2023-11-20 15:17:46 +0900

    refactor(JdbcTemplateMemberRepository): 파라미터 수정, save, findByAll 메서드 추가
    - jdbcTemplate.query(sql, memberRowMapper(), parameter)의 형태로 수정(기존에 parameter 항목을 누락시킨 채로 작성)

--- src/main/java/prao/praospring/repository/JdbcTemplateMemberRepository.java
+++ src/main/java/prao/praospring/repository/JdbcTemplateMemberRepository.java
@@ -1,41 +1,51 @@
 package prao.praospring.repository;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import javax.sql.DataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import prao.praospring.domain.Member;
 
 public class JdbcTemplateMemberRepository implements MemberRepository {
-
     private final JdbcTemplate jdbcTemplate;
 
-    //@Autowired //생성자가 1개일 경우 @Autowired 생략 가능
     public JdbcTemplateMemberRepository(DataSource dataSource) {
-        this.jdbcTemplate = new JdbcTemplate(dataSource);
+        jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
     @Override
-    public Member save(final Member member) {
-        return null;
+    public Member save(Member member) {
+        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
+        jdbcInsert.withTableName("member").usingGeneratedKeyColumns("id");
+
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("name", member.getName());
+        Number key = jdbcInsert.executeAndReturnKey(new
+                MapSqlParameterSource(parameters));
+        member.setId(key.longValue());
+        return member;
     }
 
     @Override
-    public Optional<Member> findById(final Long id) {
-        List<Member> result = jdbcTemplate.query("select * from member where id = ?", memberRowMapper());
+    public Optional<Member> findById(Long id) {
+        List<Member> result = jdbcTemplate.query("select * from member where id = ? ", memberRowMapper(), id);
         return result.stream().findAny();
     }
 
     @Override
-    public Optional<Member> findByName(final String name) {
-        List<Member> result = jdbcTemplate.query("select * from member where name = ?", memberRowMapper());
+    public Optional<Member> findByName(String name) {
+        List<Member> result = jdbcTemplate.query("select * from member where name = ? ", memberRowMapper(), name);
         return result.stream().findAny();
     }
 
     @Override
     public List<Member> findAll() {
-        return null;
+        return jdbcTemplate.query("select * from member", memberRowMapper());
     }
 
     private RowMapper<Member> memberRowMapper() {
