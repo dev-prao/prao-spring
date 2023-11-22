016399b9117a755d55a85b85eedb47b601bb7a4d
prao <pch5521@gmail.com>
2023-11-22 19:12:07 +0900

    feat(TimeTraceAop): AOP 기능 구현
    - 로직마다 동작 시간을 표시해주는 AOP 기능 구현

--- src/main/java/prao/praospring/controller/MemberController.java
+++ src/main/java/prao/praospring/controller/MemberController.java
@@ -17,6 +17,7 @@ public class MemberController {
     @Autowired
     public MemberController(final MemberService memberService) {
         this.memberService = memberService;
+        System.out.println("memberService = " + memberService.getClass());
     }
 
     @GetMapping("/members/new")
--- src/main/java/prao/praospring/service/MemberService.java
+++ src/main/java/prao/praospring/service/MemberService.java
@@ -20,17 +20,9 @@ public class MemberService {
      */
     public Long join(Member member) {
 
-        long start = System.currentTimeMillis();
-
-        try {
-            validateDuplicateMember(member); //중복 회원 검증
-            memberRepository.save(member);
-            return member.getId();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("join = " + timeMs + "ms");
-        }
+        validateDuplicateMember(member); //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
     }
 
     private void validateDuplicateMember(final Member member) {
@@ -44,15 +36,7 @@ public class MemberService {
      * 전체 회원 조회
      */
     public List<Member> findMembers() {
-        long start = System.currentTimeMillis();
-
-        try {
-            return memberRepository.findAll();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("findMembers = " + timeMs + "ms");
-        }
+        return memberRepository.findAll();
     }
 
     public Optional<Member> findOne(Long memberId) {
